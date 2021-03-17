f56e5a06e2021032f29b8c172a4316185b68a88f
Delaney <barnett21@up.edu>
2021-03-17 10:05:51 -0700

    updated gpio pin values for updated soldering

--- joey_button.py
+++ joey_button.py
@@ -4,25 +4,25 @@ import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(5,GPIO.IN)
-GPIO.setup(12,GPIO.IN)
-GPIO.setup(16,GPIO.IN)
+GPIO.setup(18,GPIO.IN)
+GPIO.setup(19,GPIO.IN)
+GPIO.setup(25,GPIO.IN)
 prev_input = 0
 
 while True:
-    leftInput = GPIO.input(5)
+    leftInput = GPIO.input(18)
     #print(leftInput)
     if (not leftInput):
         print("piece moved left")
         print(leftInput)
         time.sleep(1)
         
-    downInput = GPIO.input(12)
+    downInput = GPIO.input(25)
     if (not downInput):
         print("piece dropped")
         time.sleep(1)
 
-    rightInput = GPIO.input(16)
+    rightInput = GPIO.input(19)
     if (not rightInput):
         print("piece moved right")
         time.sleep(1)
